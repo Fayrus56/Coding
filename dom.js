2e4460d28e358966aec607336708ef289e72ec10
A Yusuf F <akuyusuf671@gmail.com>
2023-09-28 12:40:08 +0700

    belajar dom js

---
+++ dom.js
@@ -0,0 +1,9 @@
+//untuk mengatur title
+console.log(document.title); //cek dl apa title nya
+document.title = 'Yusuf Fayrus' //ini untuk mengubah title
+console.log(document.title); //mengecek title apakah sdh masuk console
+
+//untuk mengatur body
+const body = document.body //untuk mempermudah bisa langsung bikin var
+//untuk menambahkan isi di broser
+body.append('helllo world')
